8332a06b75148b93b1590cc16e9d301dfbe33207
Tom Elliott <tom@limeandsoda.co>
2023-12-05 13:40:43 +0000

    Small tidy up

--- day3/solve.ts
+++ day3/solve.ts
@@ -51,121 +51,6 @@ function parseMap(x: string, rowIdx: number): Cell[] {
   });
 }
 
-function generateNumbersAndValidLocations(
-  grid: Cell[][],
-): [Set<string>, Set<PartNumber>] {
-  // Iterate over the grid, find all the symbols and build a set of
-  // cell locations that a number must be in to be a part number
-  // Also build a list of all numbers at the same time
-
-  let validLocations = new Set<string>();
-  let numbers = new Set<PartNumber>();
-
-  grid.map((row, rowIdx) => {
-    let numberBuilder = '';
-    let numberStartCol: number | undefined = undefined;
-
-    for (let colIdx = 0; colIdx < row.length; colIdx++) {
-      const cell = grid[rowIdx][colIdx];
-      const type = cell.type;
-      if (type === 'Symbol') {
-        // First, if we were iterating over a number, store it
-        if (numberStartCol !== undefined) {
-          numbers.add({
-            row: rowIdx,
-            startCol: numberStartCol,
-            endCol: colIdx - 1,
-            length: colIdx - numberStartCol,
-            value: Number(numberBuilder),
-          });
-          numberStartCol = undefined;
-          numberBuilder = '';
-        }
-
-        // Add 8 values around cell to validLocations array
-        for (
-          let y = Math.max(0, rowIdx - 1);
-          y <= Math.min(rowIdx + 1, grid.length - 1);
-          y++
-        ) {
-          for (
-            let x = Math.max(0, colIdx - 1);
-            x <= Math.min(colIdx + 1, row.length - 1);
-            x++
-          ) {
-            if (!(x === colIdx && y === rowIdx)) {
-              validLocations.add(
-                cellPosToKey({
-                  col: x,
-                  row: y,
-                }),
-              );
-            }
-          }
-        }
-      } else if (type === 'Number') {
-        if (numberStartCol === undefined) {
-          numberStartCol = colIdx;
-        }
-        numberBuilder = numberBuilder.concat(`${cell.value}`);
-
-        // If number ends on last column, catch it
-        if (colIdx === row.length - 1) {
-          numbers.add({
-            row: rowIdx,
-            startCol: numberStartCol,
-            endCol: colIdx - 1,
-            length: colIdx - numberStartCol,
-            value: Number(numberBuilder),
-          });
-        }
-      } else if (type === 'Space') {
-        // If we were iterating over a number, store it
-        if (numberStartCol !== undefined) {
-          numbers.add({
-            row: rowIdx,
-            startCol: numberStartCol,
-            endCol: colIdx - 1,
-            length: colIdx - numberStartCol,
-            value: Number(numberBuilder),
-          });
-          numberStartCol = undefined;
-          numberBuilder = '';
-        }
-      }
-    }
-  });
-
-  return [validLocations, numbers];
-}
-
-function part1(grid: Cell[][]) {
-  const [validLocations, numbers] = generateNumbersAndValidLocations(grid);
-  // Filter numbers to only include those where they don't fall over the valid locations
-
-  const goodNumbers = Array.from(numbers).filter((partNumber) => {
-    let isValid = false;
-    for (let i = partNumber.startCol; i <= partNumber.endCol; i++) {
-      if (validLocations.has(cellPosToKey({ col: i, row: partNumber.row }))) {
-        isValid = true;
-        break;
-      }
-    }
-    return isValid;
-  });
-
-  const answer = goodNumbers.reduce(
-    (acc, partNumber) => acc + partNumber.value,
-    0,
-  );
-
-  if (answer === 530105) {
-    debugger;
-  }
-
-  return answer;
-}
-
 function findNeighbours(pos: CellPos, grid: Cell[][]): CellPos[] {
   const rowIdx = pos.row;
   const colIdx = pos.col;
@@ -207,6 +92,105 @@ function isAdjacent(partNumber: PartNumber, neighbours: CellPos[]): boolean {
     }
   }
   return false;
+}
+
+function generateNumbersAndValidLocations(
+  grid: Cell[][],
+): [Set<string>, Set<PartNumber>] {
+  // Iterate over the grid, find all the symbols and build a set of
+  // cell locations that a number must be in to be a part number
+  // Also build a list of all numbers at the same time
+
+  let validLocations = new Set<string>();
+  let numbers = new Set<PartNumber>();
+
+  grid.map((row, rowIdx) => {
+    let numberBuilder = '';
+    let numberStartCol: number | undefined = undefined;
+
+    for (let colIdx = 0; colIdx < row.length; colIdx++) {
+      const cell = grid[rowIdx][colIdx];
+      const type = cell.type;
+      if (type === 'Symbol') {
+        // First, if we were iterating over a number, store it
+        if (numberStartCol !== undefined) {
+          numbers.add({
+            row: rowIdx,
+            startCol: numberStartCol,
+            endCol: colIdx - 1,
+            length: colIdx - numberStartCol,
+            value: Number(numberBuilder),
+          });
+          numberStartCol = undefined;
+          numberBuilder = '';
+        }
+
+        const neighbours = findNeighbours(cell.pos, grid);
+        for (const neighbour of neighbours.values()) {
+          validLocations.add(
+            cellPosToKey({
+              col: neighbour.col,
+              row: neighbour.row,
+            }),
+          );
+        }
+      } else if (type === 'Number') {
+        if (numberStartCol === undefined) {
+          numberStartCol = colIdx;
+        }
+        numberBuilder = numberBuilder.concat(`${cell.value}`);
+
+        // If number ends on last column, catch it
+        if (colIdx === row.length - 1) {
+          numbers.add({
+            row: rowIdx,
+            startCol: numberStartCol,
+            endCol: colIdx - 1,
+            length: colIdx - numberStartCol,
+            value: Number(numberBuilder),
+          });
+        }
+      } else if (type === 'Space') {
+        // If we were iterating over a number, store it
+        if (numberStartCol !== undefined) {
+          numbers.add({
+            row: rowIdx,
+            startCol: numberStartCol,
+            endCol: colIdx - 1,
+            length: colIdx - numberStartCol,
+            value: Number(numberBuilder),
+          });
+          numberStartCol = undefined;
+          numberBuilder = '';
+        }
+      }
+    }
+  });
+
+  return [validLocations, numbers];
+}
+
+function part1(grid: Cell[][]) {
+  const [validLocations, numbers] = generateNumbersAndValidLocations(grid);
+
+  // Filter numbers to only include those that fall over the valid locations
+  const goodNumbers = Array.from(numbers).filter((partNumber) => {
+    let isValid = false;
+    for (let i = partNumber.startCol; i <= partNumber.endCol; i++) {
+      if (validLocations.has(cellPosToKey({ col: i, row: partNumber.row }))) {
+        isValid = true;
+        break;
+      }
+    }
+    return isValid;
+  });
+
+  const answer = goodNumbers.reduce(
+    (acc, partNumber) => acc + partNumber.value,
+    0,
+  );
+
+  return answer;
 }
 
 function part2(grid: Cell[][]) {
